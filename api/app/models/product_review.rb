1aa7a24c72b8aec7288ea033a352f350ebaf6144
Fabian Schucht <fabianschucht@gmail.com>
2022-11-26 21:31:02 +0100

    Validate uniqueness constraint on model level

--- api/app/models/product_review.rb
+++ api/app/models/product_review.rb
@@ -4,4 +4,5 @@ class ProductReview < ApplicationRecord
 
   validates :rating, presence: true, comparison: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
   validates :comment, presence: true, length: { minimum: 100 }
+  validates :user_id, uniqueness: { scope: :product_id }
 end
