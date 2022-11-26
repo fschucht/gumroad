076269ef1b059adea2e8746c87039aff506ac60a
Fabian Schucht <fabianschucht@gmail.com>
2022-11-26 17:16:57 +0100

    Add users controller

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      resources :users, only: %i[create]
       resources :products, only: %i[index show create]
     end
   end
