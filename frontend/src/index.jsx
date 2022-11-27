257e86a9fca46d714ec28c3ba6f35d28d7a23841
Fabian Schucht <fabianschucht@gmail.com>
2022-11-27 17:39:39 +0100

    Setup react-router

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode />
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
